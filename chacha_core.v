cc4b755a48607a42e2cd6c1042fe6b371c40630a
SANKALPA HOTA <62276478+Sankalpa-Hota@users.noreply.github.com>
2025-11-14 11:31:10 +0530

    Refactor initialization state in chacha_core
    Removed split counter and nonce into separate 32-bit words, using ctr and iv directly in init_state.

--- chacha_core.v
+++ chacha_core.v
@@ -1,3 +1,4 @@
+`timescale 1ns/1ps
 module chacha_core(
     input  wire clk,
     input  wire reset_n,
@@ -15,25 +16,14 @@ module chacha_core(
     reg request_pending;
     wire [511:0] chacha_out;
 
-    // split counter and nonce into 32-bit words
-    wire [31:0] ctr_lo = ctr[31:0];
-    wire [31:0] ctr_hi = ctr[63:32];
-    wire [31:0] iv0 = iv[31:0];
-    wire [31:0] iv1 = iv[63:32];
-
     wire [511:0] init_state = {
-        32'h61707865, 32'h3320646e, 32'h79622d32, 32'h6b206574, // constants
+        32'h61707865,32'h3320646e,32'h79622d32,32'h6b206574,
         key[255:224], key[223:192], key[191:160], key[159:128],
         key[127:96], key[95:64], key[63:32], key[31:0],
-        ctr_lo, ctr_hi, iv0, iv1
+        ctr, iv
     };
 
-    chacha_block BLOCK(
-        .clk(clk),
-        .rst_n(reset_n),
-        .state_in(state),
-        .state_out(chacha_out)
-    );
+    chacha_block BLOCK(.clk(clk), .rst_n(reset_n), .state_in(state), .state_out(chacha_out));
 
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n) begin
