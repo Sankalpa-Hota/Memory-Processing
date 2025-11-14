e6e0bb6625dd68b9cb506a9d8b1d9c439709da5f
SANKALPA HOTA <62276478+Sankalpa-Hota@users.noreply.github.com>
2025-11-14 10:02:58 +0530

    Refactor comments in chacha_quarterround.v
    Updated comments to clarify ChaCha quarter-round operations.

--- chacha_quarterround.v
+++ chacha_quarterround.v
@@ -4,16 +4,14 @@ module chacha_quarterround(
     output wire [31:0] a_out, b_out, c_out, d_out
 );
     wire [31:0] a1, b1, c1, d1;
-    // a += b; d ^= a; d <<<=16
-    assign a1 = a_in + b_in;
-    assign d1 = { (d_in ^ a1) [15:0], (d_in ^ a1) [31:16] }; // rotate left 16
-    // c += d; b ^= c; b <<<=12
-    assign c1 = c_in + d1;
-    assign b1 = { (b_in ^ c1) [19:0], (b_in ^ c1) [31:20] }; // rotate left 12
-    // a += b; d ^= a; d <<<=8
-    assign a_out = a1 + b1;
-    assign d_out = { (d1 ^ a_out)[23:0], (d1 ^ a_out)[31:24] }; // rotate left 8
-    // c += d; b ^= c; b <<<=7
-    assign c_out = c1 + d_out;
-    assign b_out = { (b1 ^ c_out)[24:0], (b1 ^ c_out)[31:25] }; // rotate left 7
+
+    // ChaCha quarter-round operations
+    assign a1 = a_in + b_in;                       // a += b
+    assign d1 = { (d_in ^ a1)[15:0], (d_in ^ a1)[31:16] }; // d ^= a; ROTL16
+    assign c1 = c_in + d1;                         // c += d
+    assign b1 = { (b_in ^ c1)[19:0], (b_in ^ c1)[31:20] }; // b ^= c; ROTL12
+    assign a_out = a1 + b1;                        // a += b
+    assign d_out = { (d1 ^ a_out)[23:0], (d1 ^ a_out)[31:24] }; // d ^= a; ROTL8
+    assign c_out = c1 + d_out;                     // c += d
+    assign b_out = { (b1 ^ c_out)[24:0], (b1 ^ c_out)[31:25] }; // b ^= c; ROTL7
 endmodule
