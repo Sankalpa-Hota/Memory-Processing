b8722c285e59f4a3e81fee1ad306bab1677aa4a0
SANKALPA HOTA <62276478+Sankalpa-Hota@users.noreply.github.com>
2025-11-14 11:32:08 +0530

    Update chacha_quarterround.v

--- chacha_quarterround.v
+++ chacha_quarterround.v
@@ -1,28 +1,22 @@
 // ChaCha20 32-bit quarter round
+`timescale 1ns/1ps
+
 module chacha_quarterround(
-    input  wire [31:0] a_in,
-    input  wire [31:0] b_in,
-    input  wire [31:0] c_in,
-    input  wire [31:0] d_in,
-    output wire [31:0] a_out,
-    output wire [31:0] b_out,
-    output wire [31:0] c_out,
-    output wire [31:0] d_out
+    input  wire [31:0] a_in, b_in, c_in, d_in,
+    output wire [31:0] a_out, b_out, c_out, d_out
 );
-    wire [31:0] t1, t2, t3, t4;
+    wire [31:0] a1 = a_in + b_in;
+    wire [31:0] d1 = { (d_in ^ a1) << 16 } | { (d_in ^ a1) >> (32-16) };
+    wire [31:0] c1 = c_in + d1;
+    wire [31:0] b1 = { (b_in ^ c1) << 12 } | { (b_in ^ c1) >> (32-12) };
+    wire [31:0] a2 = a1 + b1;
+    wire [31:0] d2 = { (d1 ^ a2) << 8 } | { (d1 ^ a2) >> (32-8) };
+    wire [31:0] c2 = c1 + d2;
+    wire [31:0] b2 = { (b1 ^ c2) << 7 } | { (b1 ^ c2) >> (32-7) };
 
-    assign t1 = a_in + b_in;
-    assign d_out = {((d_in ^ t1) << 16) | ((d_in ^ t1) >> 16)}; // ROTL16
-
-    assign t2 = c_in + d_out;
-    assign b_out = {((b_in ^ t2) << 12) | ((b_in ^ t2) >> 20)}; // ROTL12
-
-    assign t3 = t1 + b_out;
-    assign d_out = {((d_out ^ t3) << 8) | ((d_out ^ t3) >> 24)};  // ROTL8
-
-    assign t4 = t2 + d_out;
-    assign b_out = {((b_out ^ t4) << 7) | ((b_out ^ t4) >> 25)};  // ROTL7
-
-    assign a_out = t3;
-    assign c_out = t4;
+    assign a_out = a2;
+    assign b_out = b2;
+    assign c_out = c2;
+    assign d_out = d2;
 endmodule
+
